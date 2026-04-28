2c33526cbbf478337ba8d66a481b1c3492c4188e
Matheus <matheuswschneider@gmail.com>
2026-04-28 13:14:35 -0300

    Fixing Tarefafor4

--- src/TarefaFor4.java
+++ src/TarefaFor4.java
@@ -8,8 +8,18 @@ public class TarefaFor4 {
         System.out.println("informe a quantidade de numeros da sequência de Fibonacci você gostaria de conhecer:");
         int numero = sc.nextInt();
 
+        String fibonacci = "";
+        
+        int a= 0; 
+        int b =1;
         for(int i=0;i<numero;i++){
-            
+            fibonacci = fibonacci +a+ " ";
+            int proximo = a+b;
+            a= b;
+            b =proximo;
+
         }
+        System.out.println(fibonacci);
     }
+
 }
